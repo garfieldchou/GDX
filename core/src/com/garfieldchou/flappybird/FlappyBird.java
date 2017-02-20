5e262cd27cc51e432ba07b0eec536bcf4efb4084
shaudai <shaudai0402@hotmail.com>
2017-02-21 07:34:06 +0800

    Put the bird at the center of the background

--- core/src/com/garfieldchou/flappybird/FlappyBird.java
+++ core/src/com/garfieldchou/flappybird/FlappyBird.java
@@ -9,11 +9,13 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class FlappyBird extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture background;
+	Texture bird;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		background = new Texture("bg.png");
+		bird = new Texture("bird.png");
 	}
 
 	@Override
@@ -21,6 +23,7 @@ public class FlappyBird extends ApplicationAdapter {
 
 		batch.begin();
 		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		batch.draw(bird, Gdx.graphics.getWidth() / 2 - bird.getWidth() / 2, Gdx.graphics.getHeight() / 2 - bird.getHeight() / 2);
 		batch.end();
 
 	}
